694336684948061e19b17ee1e5649272cfca41e7
Gregory Vincic <g@7de.se>
2022-08-04 08:08:56 +0200

    Refactor

--- fixhead.go
+++ fixhead.go
@@ -19,3 +19,23 @@ func (h FixedHeader) HasFlag(f byte) bool {
 func (h FixedHeader) Flags() byte {
 	return byte(h[0]) & 0b0000_1111
 }
+
+// FixedHeader flags
+const (
+	DUP    byte = 0b0000_1000
+	RETAIN byte = 0b0000_0001
+
+	QoS0 byte = 0b0000_0000
+	QoS1 byte = 0b0000_0010
+	QoS2 byte = 0b0000_0100
+	//QoS3 FixedHeader = 0b0000_0110   malformed!
+)
+
+var fixedHeaderFlagNames = map[byte]string{
+	DUP:    "DUP",
+	RETAIN: "RETAIN",
+
+	QoS0: "QoS0",
+	QoS1: "QoS1",
+	QoS2: "QoS2",
+}
