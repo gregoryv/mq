a6ee32b38252bf03055811ec26c5bda7a75fa5a2
Gregory Vincic <g@7de.se>
2022-08-06 16:20:43 +0200

    Refactor

--- ctrlpack.go
+++
@@ -1,6 +0,0 @@
-package mqtt
-
-// https://docs.oasis-open.org/mqtt/mqtt/v5.0/os/mqtt-v5.0-os.html#_MQTT_Control_Packet
-type ControlPacket interface {
-	FixedHeader() FixedHeader
-}
--- package.go
+++ package.go
@@ -17,6 +17,11 @@ import (
 	"github.com/gregoryv/mqtt/internal"
 )
 
+// https://docs.oasis-open.org/mqtt/mqtt/v5.0/os/mqtt-v5.0-os.html#_MQTT_Control_Packet
+type ControlPacket interface {
+	FixedHeader() FixedHeader
+}
+
 // 2.1.2 MQTT Control Packet type
 //
 // https://docs.oasis-open.org/mqtt/mqtt/v5.0/os/mqtt-v5.0-os.html#_MQTT_Control_Packet
