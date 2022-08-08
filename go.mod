99ef3b91f6ac49b1d3de56d07f75d30231e74927
Gregory Vincic <g@7de.se>
2022-08-08 19:35:38 +0200

    Remove type Connect and func Parse

--- go.mod
+++ go.mod
@@ -1,8 +1,3 @@
 module github.com/gregoryv/mqtt
 
 go 1.18
-
-require (
-	github.com/gregoryv/asserter v0.4.2 // indirect
-	github.com/pmezard/go-difflib v1.0.0 // indirect
-)
--- package.go
+++ package.go
@@ -10,10 +10,6 @@ https://docs.oasis-open.org/mqtt/mqtt/v5.0/os/mqtt-v5.0-os.html
 package mqtt
 
 import (
-	"io"
-	"io/ioutil"
-	"log"
-
 	"github.com/gregoryv/mqtt/internal"
 )
 
@@ -113,10 +109,3 @@ var codeNames = map[byte]string{
 	SubscriptionIdentifiersNotSupported: "Subscription Identifiers not supported",
 	WildcardSubscriptionsNotSupported:   "Wildcard Subscriptions not supported",
 }
-
-// ----------------------------------------
-
-// SetOutput of debug logs. By default debug output is discarded.
-func SetOutput(w io.Writer) { debug.SetOutput(w) }
-
-var debug = log.New(ioutil.Discard, "mqtt ", log.Lshortfile|log.Lmsgprefix)
