1da882689ab63dfeee72f30770382f94b9c79fdf
Gregory Vincic <g@7de.se>
2022-08-12 15:52:33 +0200

    Rename type FixedHeader -> Fixed

--- package.go
+++ package.go
@@ -19,72 +19,36 @@ import (
 // Headers
 // ---------------------------------------------------------------------
 
-// FixedHeader represents the first 2..5 bytes of a control packet.
-
-// It's an error if len(FixedHeader) < 2 or > 5.
+// Fixed represents the first byte in a control packet.
+//
 //
 // 2.1.1 Fixed Header
 // https://docs.oasis-open.org/mqtt/mqtt/v5.0/os/mqtt-v5.0-os.html#_MQTT_Control_Packet
-type FixedHeader struct {
-	header       byte
-	remainingLen vbint
-}
-
-func (f *FixedHeader) MarshalBinary() ([]byte, error) {
-	data := make([]byte, 0, 5)
-	data = append(data, f.header)
-	rem, _ := f.remainingLen.MarshalBinary() // cannot fail
-	data = append(data, rem...)
-	return data, nil
-}
-
-func (f *FixedHeader) UnmarshalBinary(data []byte) error {
-	f.header = data[0]
-	err := f.remainingLen.UnmarshalBinary(data[1:])
-	if err != nil {
-		return unmarshalErr(f, "remaining length", err.(*Malformed))
-	}
-	return nil
-}
+type Fixed byte
 
 // String returns a string TYPE-FLAGS REMAINING_LENGTH
-func (f *FixedHeader) String() string {
+func (f Fixed) String() string {
 	var sb strings.Builder
-	sb.WriteString(typeNames[f.Value()])
+	sb.WriteString(typeNames[byte(f)&0b1111_0000])
 	sb.WriteString(" ")
 	flags := []byte("----")
-	if f.HasFlag(DUP) {
+	if bits(f).Has(DUP) {
 		flags[0] = 'd'
 	}
 	switch {
-	case f.HasFlag(QoS1 | QoS2):
+	case bits(f).Has(QoS1 | QoS2):
 		flags[1] = '!' // malformed
 		flags[2] = '!' // malformed
-	case f.HasFlag(QoS1):
+	case bits(f).Has(QoS1):
 		flags[2] = '1'
-	case f.HasFlag(QoS2):
+	case bits(f).Has(QoS2):
 		flags[1] = '2'
 	}
-	if f.HasFlag(RETAIN) {
+	if bits(f).Has(RETAIN) {
 		flags[3] = 'r'
 	}
 	sb.Write(flags)
-	sb.WriteString(" ")
-	fmt.Fprint(&sb, f.remainingLen)
 	return sb.String()
-}
-
-// Is is the same as h.Value() == v
-func (f *FixedHeader) Is(v byte) bool {
-	return f.Value() == v
-}
-
-func (f *FixedHeader) Value() byte {
-	return f.header & 0b1111_0000
-}
-
-func (f *FixedHeader) HasFlag(flag byte) bool {
-	return bits(f.header).Has(flag)
 }
 
 // ---------------------------------------------------------------------
@@ -415,7 +379,7 @@ const (
 	QoS0   byte = 0b0000_0000
 	QoS1   byte = 0b0000_0010
 	QoS2   byte = 0b0000_0100
-	//QoS3 FixedHeader = 0b0000_0110   malformed!
+	//QoS3 Fixed = 0b0000_0110   malformed!
 	DUP byte = 0b0000_1000
 )
 
--- package_test.go
+++ package_test.go
@@ -13,57 +13,16 @@ import (
 // Headers
 // ---------------------------------------------------------------------
 
-func ExampleFixedHeader() {
-	bad := []byte{PUBLISH | QoS1 | QoS2}
-	var f FixedHeader
-	fmt.Println(f.UnmarshalBinary(bad))
-	// output:
-	// malformed mqtt.FixedHeader unmarshal: remaining length missing data
-}
-
-func TestFixedHeader(t *testing.T) {
-	b := FixedHeader{
-		header:       PUBLISH | DUP | QoS1,
-		remainingLen: 10,
-	}
-	// marshaling
-	data, err := b.MarshalBinary()
-	if err != nil {
-		t.Error("MarshalBinary", err)
-	}
-
-	var a FixedHeader
-	if err := a.UnmarshalBinary(data); err != nil {
-		t.Error("UnmarshalBinary", err)
-	}
-
-	// other methods
-	if a.Is(CONNECT) {
-		t.Error("!Is", CONNECT)
-	}
-	if a.HasFlag(RETAIN) {
-		t.Error("!HasFlag", RETAIN)
-	}
+func TestFixed(t *testing.T) {
 	cases := []struct {
-		h   FixedHeader
+		h   Fixed
 		exp string
 	}{
-		{
-			h:   a,
-			exp: "PUBLISH d-1- 10",
-		},
-		{
-			h:   FixedHeader{header: PUBLISH | QoS2 | RETAIN},
-			exp: "PUBLISH -2-r 0",
-		},
-		{
-			h:   FixedHeader{header: PUBLISH | QoS1 | QoS2},
-			exp: "PUBLISH -!!- 0",
-		},
-		{
-			h:   FixedHeader{header: CONNECT},
-			exp: "CONNECT ---- 0",
-		},
+		{Fixed(PUBLISH | QoS2 | RETAIN), "PUBLISH -2-r"},
+		{Fixed(PUBLISH | QoS1 | QoS2), "PUBLISH -!!-"},
+		{Fixed(PUBLISH | DUP | QoS2), "PUBLISH d2--"},
+		{Fixed(PUBLISH | QoS1), "PUBLISH --1-"},
+		{Fixed(CONNECT), "CONNECT ----"},
 	}
 	for _, c := range cases {
 		if got, exp := c.h.String(), c.exp; got != exp {
