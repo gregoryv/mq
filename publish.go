4acb77c8802e53d36fd10344f2f306bae04b1d25
Gregory Vincic <g@7de.se>
2022-08-21 14:43:18 +0200

    Start implementing Publish.WriteTo

--- publish.go
+++ publish.go
@@ -1,24 +1,68 @@
 package mqtt
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+)
 
 func NewPublish() *Publish {
-	return &Publish{}
+	return &Publish{
+		fixed: Bits(PUBLISH),
+	}
 }
 
 type Publish struct {
-	fixed Bits
+	fixed     Bits
+	topicName wstring
+
+	packetIdent           wuint16
+	topicAlias            wuint16
+	messageExpiryInterval wuint32
+	responseTopic         wstring
+	correlationData       bindata
+	userProp              []property
+	subIdentifiers        []vbint
+	contentType           wstring
+
+	payloadFormat wbool
+	payload       bindata
 }
 
 func (p *Publish) UnmarshalBinary(data []byte) error {
 	return fmt.Errorf(": todo")
 }
 
+func (p *Publish) WriteTo(w io.Writer) (int64, error) {
+	// allocate full size of entire packet
+	b := make([]byte, p.fill(_LEN, 0))
+	p.fill(b, 0)
+
+	n, err := w.Write(b)
+	return int64(n), err
+}
+
 func (p *Publish) fill(b []byte, i int) int {
-	panic("implement Publish.fill")
-	return -1
+	remainingLen := vbint(
+		p.variableHeader(_LEN, 0) + p.payload.fill(_LEN, 0),
+	)
+
+	i += p.fixed.fill(b, i)      // firstByte header
+	i += remainingLen.fill(b, i) // remaining length
+	i += p.variableHeader(b, i)  // variable header
+	i += p.payload.fill(b, i)    // payload
+
+	return i
+}
+func (p *Publish) variableHeader(b []byte, i int) int {
+	n := i
+	// todo
+	return i - n
 }
 
 func (p *Publish) width() int {
 	return p.fill(_LEN, 0)
+}
+
+func (p *Publish) String() string {
+	return fmt.Sprintf("%s", firstByte(p.fixed).String())
 }
--- publish_test.go
+++ publish_test.go
@@ -1,9 +1,23 @@
 package mqtt
 
-import "testing"
+import (
+	"bytes"
+	"testing"
+	"unsafe"
+)
 
 var _ wireType = &Publish{}
 
-func Test(t *testing.T) {
-	_ = NewPublish()
+func TestPublish(t *testing.T) {
+	p := NewPublish()
+	t.Log(p, unsafe.Sizeof(p))
+
+	var buf bytes.Buffer
+	if _, err := p.WriteTo(&buf); err != nil {
+		t.Fatal(err)
+	}
+
+	if got := p.width(); got < 0 {
+		t.Error(got)
+	}
 }
