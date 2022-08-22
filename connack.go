1ad3aabfadd5fe5485bda3ecfa5669b9cd80ba19
Gregory Vincic <g@7de.se>
2022-08-22 21:14:08 +0200

    Add Connack field methods

--- connack.go
+++ connack.go
@@ -1,30 +1,139 @@
 package mqtt
 
+import (
+	"bytes"
+	"fmt"
+)
+
 func NewConnAck() *ConnAck {
-	return &ConnAck{}
+	return &ConnAck{
+		fixed: Bits(CONNACK),
+	}
 }
 
 type ConnAck struct {
 	fixed      Bits
-	flags      Bits
+	flags      Bits // sessionPresent as 7-1 are reserved
 	reasonCode wuint8
 
+	// properties
 	sessionExpiryInterval wuint32
 	receiveMax            wuint16
 	maxQoS                wuint8 // 0 or 1, 2
-	retainAvailable       wuint8
+	retainAvailable       wbool
 	maxPacketSize         wuint32
 	assignedClientID      wstring
 	topicAliasMax         wuint16
 	reasonString          wstring
 
-	userProp                         []property
-	WildcardSubscriptionAvailable    wbool
-	SubscriptionIdentifiersAvailable wbool
-	SharedSubscriptionAvailable      wbool
-	ServerKeepAlive                  wuint16
-	ResponseInformation              wstring
-	ServerReference                  wstring
-	AuthenticationMethod             wstring
-	AuthenticationData               bindata
+	userProp                []property
+	wildcardSubAvailable    wbool
+	subIdentifiersAvailable wbool
+	sharedSubAvailable      wbool
+	serverKeepAlive         wuint16
+	responseInformation     wstring
+	serverReference         wstring
+	authMethod              wstring
+	authData                bindata
+}
+
+func (c *ConnAck) Flags() Bits         { return c.flags }
+func (c *ConnAck) HasFlag(v byte) bool { return c.flags.Has(v) }
+
+func (c *ConnAck) SetSessionPresent(v bool) { c.flags.toggle(1, true) }
+func (c *ConnAck) SessionPresent() bool     { return c.flags.Has(1) }
+
+func (c *ConnAck) SetSessionExpiryInterval(v uint32) {
+	c.sessionExpiryInterval = wuint32(v)
+}
+func (c *ConnAck) SessionExpiryInterval() uint32 {
+	return uint32(c.sessionExpiryInterval)
+}
+
+func (c *ConnAck) SetReceiveMax(v uint16) { c.receiveMax = wuint16(v) }
+func (c *ConnAck) ReceiveMax() uint16     { return uint16(c.receiveMax) }
+
+func (c *ConnAck) SetMaxQoS(v uint8) { c.maxQoS = wuint8(v) }
+func (c *ConnAck) MaxQoS() uint8     { return uint8(c.maxQoS) }
+
+func (c *ConnAck) SetRetainAvailable(v bool) { c.retainAvailable = wbool(v) }
+func (c *ConnAck) RetainAvailable() bool     { return bool(c.retainAvailable) }
+
+func (c *ConnAck) SetMaxPacketSize(v uint32) { c.maxPacketSize = wuint32(v) }
+func (c *ConnAck) MaxPacketSize() uint32     { return uint32(c.maxPacketSize) }
+
+func (c *ConnAck) SetAssignedClientID(v string) { c.assignedClientID = wstring(v) }
+func (c *ConnAck) AssignedClientID() string     { return string(c.assignedClientID) }
+
+func (c *ConnAck) SetTopicAliasMax(v uint16) { c.topicAliasMax = wuint16(v) }
+func (c *ConnAck) TopicAliasMax() uint16     { return uint16(c.topicAliasMax) }
+
+func (c *ConnAck) SetReasonString(v string) { c.reasonString = wstring(v) }
+func (c *ConnAck) ReasonString() string     { return string(c.reasonString) }
+
+// AddUserProp adds a user property. The User Property is allowed to
+// appear multiple times to represent multiple name, value pairs. The
+// same name is allowed to appear more than once.
+func (c *ConnAck) AddUserProp(key, val string) {
+	c.AddUserProperty(property{key, val})
+}
+func (c *ConnAck) AddUserProperty(p property) {
+	c.appendUserProperty(p)
+}
+func (c *ConnAck) appendUserProperty(p property) {
+	c.userProp = append(c.userProp, p)
+}
+
+// end settings
+// ----------------------------------------
+
+func (c *ConnAck) String() string {
+	return fmt.Sprintf("%s %s %s %v bytes",
+		FirstByte(c.fixed).String(),
+		connAckFlags(c.flags),
+		c.assignedClientID,
+		c.fill(_LEN, 0),
+	)
+}
+
+func (c *ConnAck) UnmarshalBinary(data []byte) error {
+	return fmt.Errorf(": todo")
+}
+
+func (c *ConnAck) fill(b []byte, i int) int {
+	return -1
+}
+
+func (c *ConnAck) width() int {
+	return c.fill(_LEN, 0)
+}
+
+// ----------------------------------------
+
+type connAckFlags byte
+
+// String returns flags represented with a letter.
+// Improper flags are marked with '!' and unset are marked with '-'.
+//
+//   SessionPresent s
+//   Reserved      !
+func (c connAckFlags) String() string {
+	flags := bytes.Repeat([]byte("-"), 8)
+
+	mark := func(i int, flag byte, v byte) {
+		if !Bits(c).Has(flag) {
+			return
+		}
+		flags[i] = v
+	}
+	mark(0, 1<<7, '!')
+	mark(1, 1<<6, '!')
+	mark(2, 1<<5, '!')
+	mark(3, 1<<4, '!')
+	mark(4, 1<<3, '!')
+	mark(5, 1<<2, '!')
+	mark(6, 1<<1, '!')
+	mark(7, 1<<0, 's')
+
+	return string(flags) // + fmt.Sprintf(" %08b", c)
 }
--- connack_test.go
+++ connack_test.go
@@ -3,5 +3,22 @@ package mqtt
 import "testing"
 
 func TestConnAck(t *testing.T) {
-	_ = NewConnAck()
+	a := NewConnAck()
+
+	eq(t, a.SetSessionPresent, a.SessionPresent, true)
+
+	eq(t, a.SetSessionExpiryInterval, a.SessionExpiryInterval, 199)
+	eq(t, a.SetReceiveMax, a.ReceiveMax, 81)
+	eq(t, a.SetMaxQoS, a.MaxQoS, 1)
+	eq(t, a.SetRetainAvailable, a.RetainAvailable, true)
+	eq(t, a.SetMaxPacketSize, a.MaxPacketSize, 250)
+	eq(t, a.SetAssignedClientID, a.AssignedClientID, "macy")
+	eq(t, a.SetTopicAliasMax, a.TopicAliasMax, 11)
+	eq(t, a.SetReasonString, a.ReasonString, "because")
+
+	a.AddUserProp("color", "red")
+
+	t.Logf("\n\n%s\n\n", a)
 }
+
+var _ wireType = &ConnAck{}
