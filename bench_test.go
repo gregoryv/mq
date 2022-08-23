2af7d0d1393624d94ee8b5c6cf2aa651f48cc10a
Gregory Vincic <g@7de.se>
2022-08-23 05:26:30 +0200

    Refactor

---
+++ bench_test.go
@@ -0,0 +1,129 @@
+package mqtt
+
+import (
+	"io/ioutil"
+	"testing"
+
+	"github.com/eclipse/paho.golang/packets"
+)
+
+func BenchmarkCreateAndWriteTo(b *testing.B) {
+	var (
+		alive   = uint16(30)
+		cid     = "macy"
+		user    = "john.doe"
+		pwd     = []byte("secret")
+		sExpiry = uint32(30)
+	)
+
+	b.Run("our", func(b *testing.B) {
+		for n := 0; n < b.N; n++ {
+
+			// our packet
+			our := NewConnect()
+			our.SetKeepAlive(alive)
+			our.SetClientID(cid)
+			our.SetUsername(user)
+			our.SetPassword(pwd)
+			our.SetSessionExpiryInterval(sExpiry)
+			our.WriteTo(ioutil.Discard)
+		}
+	})
+
+	b.Run("their", func(b *testing.B) {
+		for n := 0; n < b.N; n++ {
+			their := packets.NewControlPacket(packets.CONNECT)
+			c := their.Content.(*packets.Connect)
+			c.KeepAlive = alive
+			c.ClientID = cid
+			c.UsernameFlag = true
+			c.Username = user
+			c.PasswordFlag = true
+			c.Password = pwd
+			c.Properties.SessionExpiryInterval = &sExpiry
+			their.WriteTo(ioutil.Discard)
+		}
+	})
+}
+
+func BenchmarkNewConnect(b *testing.B) {
+	var (
+		alive   = uint16(30)
+		cid     = "macy"
+		user    = "john.doe"
+		pwd     = []byte("secret")
+		sExpiry = uint32(30)
+	)
+
+	b.Run("our", func(b *testing.B) {
+		for n := 0; n < b.N; n++ {
+
+			// our packet
+			our := NewConnect()
+			our.SetKeepAlive(alive)
+			our.SetClientID(cid)
+			our.SetUsername(user)
+			our.SetPassword(pwd)
+			our.SetSessionExpiryInterval(sExpiry)
+		}
+	})
+
+	b.Run("their", func(b *testing.B) {
+		for n := 0; n < b.N; n++ {
+			their := packets.NewControlPacket(packets.CONNECT)
+			c := their.Content.(*packets.Connect)
+			c.KeepAlive = alive
+			c.ClientID = cid
+			c.UsernameFlag = true
+			c.Username = user
+			c.PasswordFlag = true
+			c.Password = pwd
+			c.Properties.SessionExpiryInterval = &sExpiry
+		}
+	})
+
+}
+
+func BenchmarkConnect_WriteTo(b *testing.B) {
+	var (
+		our   *Connect
+		their *packets.ControlPacket
+
+		alive   = uint16(30)
+		cid     = "macy"
+		user    = "john.doe"
+		pwd     = []byte("secret")
+		sExpiry = uint32(30)
+	)
+
+	// our packet
+	our = NewConnect()
+	our.SetKeepAlive(alive)
+	our.SetClientID(cid)
+	our.SetUsername(user)
+	our.SetPassword(pwd)
+	our.SetSessionExpiryInterval(sExpiry)
+
+	// their packet
+	their = packets.NewControlPacket(packets.CONNECT)
+	c := their.Content.(*packets.Connect)
+	c.KeepAlive = alive
+	c.ClientID = cid
+	c.UsernameFlag = true
+	c.Username = user
+	c.PasswordFlag = true
+	c.Password = pwd
+	c.Properties.SessionExpiryInterval = &sExpiry
+
+	b.Run("our", func(b *testing.B) {
+		for n := 0; n < b.N; n++ {
+			our.WriteTo(ioutil.Discard)
+		}
+	})
+
+	b.Run("their", func(b *testing.B) {
+		for n := 0; n < b.N; n++ {
+			their.WriteTo(ioutil.Discard)
+		}
+	})
+}
