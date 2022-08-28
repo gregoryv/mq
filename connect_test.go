be11d4fa6ff359c38665b945c6181318e734c791
Gregory Vincic <g@7de.se>
2022-08-28 18:03:46 +0200

    Fix Connect flags

--- connect_test.go
+++ connect_test.go
@@ -1,7 +1,6 @@
 package mqtt
 
 import (
-	"bytes"
 	"fmt"
 	"reflect"
 	"strings"
@@ -21,28 +20,24 @@ func ExampleConnect() {
 }
 
 func TestConnect(t *testing.T) {
-	//
 	c := NewConnect()
 	eq(t, c.SetProtocolVersion, c.ProtocolVersion, 5)
 	eq(t, c.SetProtocolName, c.ProtocolName, "MQTT")
-
 	eq(t, c.SetKeepAlive, c.KeepAlive, 299)
 	eq(t, c.SetClientID, c.ClientID, "macy")
 	eq(t, c.SetSessionExpiryInterval, c.SessionExpiryInterval, 30)
-
 	eq(t, c.SetUsername, c.Username, "john.doe")
 	eq(t, c.SetPassword, c.Password, []byte("123"))
 	eq(t, c.SetAuthMethod, c.AuthMethod, "digest")
 	eq(t, c.SetAuthData, c.AuthData, []byte("secret"))
-
-	c.AddUserProp("color", "red")
-
 	eq(t, c.SetMaxPacketSize, c.MaxPacketSize, 4096)
 	eq(t, c.SetTopicAliasMax, c.TopicAliasMax, 128)
 	eq(t, c.SetRequestResponseInfo, c.RequestResponseInfo, true)
 	eq(t, c.SetRequestProblemInfo, c.RequestProblemInfo, true)
 	eq(t, c.SetResponseTopic, c.ResponseTopic, "response/to/macy")
 	eq(t, c.SetCorrelationData, c.CorrelationData, []byte("perhaps a uuid"))
+
+	c.AddUserProp("color", "red")
 
 	eq(t, c.SetWillRetain, c.WillRetain, true)
 	eq(t, c.SetWillTopic, c.WillTopic, "topic/dead/clients")
@@ -52,10 +47,6 @@ func TestConnect(t *testing.T) {
 	eq(t, c.SetWillPayloadFormat, c.WillPayloadFormat, true)
 	eq(t, c.SetWillMessageExpiryInterval, c.WillMessageExpiryInterval, 100)
 	c.AddWillProp("connected", "2022-01-01 14:44:32")
-
-	var buf bytes.Buffer
-	c.WriteTo(&buf)
-	//t.Logf("\n\n%s\n\n%s\n\n", c, hex.Dump(buf.Bytes()))
 
 	if got := c.String(); !strings.Contains(got, "CONNECT") {
 		t.Error(got)
@@ -84,19 +75,19 @@ func eq[T any](t *testing.T, set func(T), get func() T, value T) {
 	}
 }
 
-func TestconnectFlags(t *testing.T) {
+func Test_connectFlags(t *testing.T) {
 	f := connectFlags(0b11110110)
 	// QoS2
-	if got, exp := f.String(), "upr2ws-"; got != exp {
+	if got, exp := f.String(), "upr2-ws-"; got != exp {
 		t.Errorf("got %q != exp %q", got, exp)
 	}
 	// QoS1
 	f = connectFlags(0b11101110)
-	if got, exp := f.String(), "upr1ws-"; got != exp {
+	if got, exp := f.String(), "upr-1ws-"; got != exp {
 		t.Errorf("got %q != exp %q", got, exp)
 	}
 	f = connectFlags(0b00000001)
-	if got, exp := f.String(), "------!"; got != exp {
+	if got, exp := f.String(), "-------!"; got != exp {
 		t.Errorf("got %q != exp %q", got, exp)
 	}
 }
