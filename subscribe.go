0e35da9c76a12cd5109a04cc07581ec356c06347
Gregory Vincic <g@7de.se>
2022-08-29 22:26:20 +0200

    Add subscribe fields

--- subscribe.go
+++ subscribe.go
@@ -1,9 +1,35 @@
 package mqtt
 
+import "fmt"
+
 func NewSubscribe() Subscribe {
-	return Subscribe{}
+	return Subscribe{fixed: Bits(SUBSCRIBE)}
 }
 
 type Subscribe struct {
-	fixed Bits
+	fixed          Bits
+	packetID       wuint16
+	subscriptionID vbint
+	userProp       []property
+	filters        []TopicFilter
+}
+
+func (p *Subscribe) String() string {
+	return fmt.Sprintf("%s %v",
+		firstByte(p.fixed).String(),
+		p.packetID,
+	)
+}
+
+func (p *Subscribe) SetPacketID(v uint16) { p.packetID = wuint16(v) }
+func (p *Subscribe) PacketID() uint16     { return uint16(p.packetID) }
+
+func (p *Subscribe) SetSubscriptionID(v int) { p.subscriptionID = vbint(v) }
+func (p *Subscribe) SubscriptionID() int     { return int(p.subscriptionID) }
+
+// ----------------------------------------
+
+type TopicFilter struct {
+	filter  wstring
+	options Bits
 }
--- subscribe_test.go
+++ subscribe_test.go
@@ -3,5 +3,10 @@ package mqtt
 import "testing"
 
 func TestSubscribe(t *testing.T) {
-	_ = NewSubscribe()
+	s := NewSubscribe()
+
+	eq(t, s.SetPacketID, s.PacketID, 34)
+	eq(t, s.SetSubscriptionID, s.SubscriptionID, 99)
+
+	t.Error(&s)
 }
