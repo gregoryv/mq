3dab627ed586ef8a49ca2c4eeaa69fc048e755ed
Gregory Vincic <g@7de.se>
2022-08-29 21:07:09 +0200

    Refactor PubAck

--- puback.go
+++ puback.go
@@ -52,10 +52,8 @@ func (p *PubAck) AddUserProperty(prop property) {
 }
 
 func (p *PubAck) WriteTo(w io.Writer) (int64, error) {
-	// allocate full size of entire packet
 	b := make([]byte, p.fill(_LEN, 0))
 	p.fill(b, 0)
-
 	n, err := w.Write(b)
 	return int64(n), err
 }
@@ -66,11 +64,9 @@ func (p *PubAck) width() int {
 
 func (p *PubAck) fill(b []byte, i int) int {
 	remainingLen := vbint(p.variableHeader(_LEN, 0))
-
 	i += p.fixed.fill(b, i)      // firstByte header
 	i += remainingLen.fill(b, i) // remaining length
 	i += p.variableHeader(b, i)
-
 	return i
 }
 func (p *PubAck) variableHeader(b []byte, i int) int {
@@ -98,20 +94,15 @@ func (p *PubAck) properties(b []byte, i int) int {
 }
 
 func (p *PubAck) UnmarshalBinary(data []byte) error {
-	buf := &buffer{data: data}
-	get := buf.get
-
-	get(&p.packetID)
-
+	b := &buffer{data: data}
+	b.get(&p.packetID)
 	// no more data, see 3.4.2.1 PUBACK Reason Code
-	if len(data) == buf.i {
-		return buf.err
+	if len(data) == b.i {
+		return b.err
 	}
-
-	get(&p.reasonCode)
-	buf.getAny(p.propertyMap(), p.AddUserProperty)
-
-	return buf.err
+	b.get(&p.reasonCode)
+	b.getAny(p.propertyMap(), p.AddUserProperty)
+	return b.err
 }
 
 func (p *PubAck) propertyMap() map[Ident]wireType {
