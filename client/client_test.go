fa0e74f45be85c0896bb090746c5de7f0fcc5202
Gregory Vincic <g@7de.se>
2022-09-04 06:59:32 +0200

    Use eclipse-mosquitto broker to test client

--- client/client_test.go
+++ client/client_test.go
@@ -1,16 +1,52 @@
 package client
 
 import (
+	"net"
 	"testing"
 
 	"github.com/gregoryv/mqtt"
 )
 
 func TestClient(t *testing.T) {
-	c := mqtt.NewConnect()
-	c.Flags().Has(mqtt.CleanStart)
-	// above needs knowledge of the flags and the protocol
-	//
-	// nicer, not having to know how it's stored and where
-	// c.CleanStart()
+	// dial broker
+	conn, err := net.Dial("tcp", "127.0.0.1:1883")
+	if err != nil {
+		t.Log("no broker, did you run docker-compose up?")
+		t.Fatal(err)
+	}
+
+	// disconnect nicely
+	defer func() {
+		p := mqtt.NewDisconnect()
+		t.Log(&p)
+		p.WriteTo(conn)
+	}()
+
+	// connect mqtt client
+	{
+		p := mqtt.NewConnect()
+		p.SetClientID("macy")
+		p.WriteTo(conn)
+		t.Log(&p)
+
+		// check that it's acknowledged
+		a, err := mqtt.ReadPacket(conn)
+		if err != nil {
+			t.Fatal(err)
+		}
+		t.Log(a)
+		if _, ok := a.(*mqtt.ConnAck); !ok {
+			t.Fatal("no ConnAck")
+		}
+	}
+
+	{
+		p := mqtt.NewPublish()
+		p.SetPacketID(99)
+		p.SetRetain(true)
+		p.SetTopicName("a/b/1")
+		p.SetPayload([]byte("gopher"))
+		p.WriteTo(conn)
+		t.Log(&p)
+	}
 }
