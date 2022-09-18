7641ff927b49bdf88d613b31d379dc584fddaeb6
Gregory Vincic <g@7de.se>
2022-09-18 03:36:37 +0200

    Fix TestIDPool

--- client/idpool_test.go
+++ client/idpool_test.go
@@ -12,14 +12,19 @@ func TestIDPool(t *testing.T) {
 	p.Next(ctx) // 1
 	p.Next(ctx) // 2
 	p.Reuse(2)
-	if v := p.Next(ctx); v != 2 {
-		t.Error(v)
+	if v := p.Next(ctx); v != 3 {
+		t.Error("unexpected next packet id", v)
 	}
 
 	// check waiting for
-	p.Next() // 3
+	p.Next(ctx) // 3
 	go p.Reuse(3)
 	if v := p.Next(ctx); v != 3 {
 		t.Error(v)
+	}
+
+	p.Reuse(1)
+	if v := p.Next(ctx); v != 1 {
+		t.Error("unexpected packet id", v)
 	}
 }
