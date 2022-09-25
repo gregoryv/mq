8b6ff05a8988195aedb7a7dd06712fa7d1779578
Gregory Vincic <g@7de.se>
2022-09-25 16:02:08 +0200

    Update TestThingClient

--- client/client_test.go
+++ client/client_test.go
@@ -10,8 +10,10 @@ import (
 	"github.com/gregoryv/mqtt"
 )
 
+// thing is anything like an iot device that mostly sends stats to the
+// cloud
 func TestThingClient(t *testing.T) {
-	// dial broker
+	//dial broker
 	conn, err := net.Dial("tcp", "127.0.0.1:1883")
 	if err != nil {
 		t.Log("no broker, did you run docker-compose up?")
@@ -63,10 +65,22 @@ func TestAppClient(t *testing.T) {
 	}
 	{ // subscribe
 		p := mqtt.NewSubscribe()
-		p.AddFilter("a/b", mqtt.FopQoS1|mqtt.FopNL)
+		p.AddFilter("a/b", mqtt.FopQoS1)
 		if err := c.Subscribe(ctx, &p); err != nil {
 			t.Fatal(err)
 		}
+	}
+	// todo use a client to send a message on the subscribed topic
+	// wip, need to implement routing of subscribed filters in
+	// previous step and assert that the message arrives properly.
+	{
+		// publish application message
+		p := mqtt.NewPublish()
+		p.SetQoS(2)
+		p.SetTopicName("a/b")
+		p.SetPayload([]byte("gopher"))
+		c.Publish(ctx, &p)
+		<-time.After(50 * time.Millisecond)
 	}
 	{ // disconnect nicely
 		p := mqtt.NewDisconnect()
