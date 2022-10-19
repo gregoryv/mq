150523c10723d3ddd5fce15c631b5f33fc2890b4
Gregory Vincic <gregoryvse@gmail.com>
2022-10-19 08:00:48 +0200

    Add tt/pakio

---
+++ tt/pakio/sender.go
@@ -0,0 +1,27 @@
+package pakio
+
+import (
+	"context"
+	"io"
+	"sync"
+
+	"github.com/gregoryv/mq"
+)
+
+func NewSender(v io.Writer) *Sender {
+	return &Sender{Writer: v}
+}
+
+type Sender struct {
+	sync.Mutex
+	io.Writer
+}
+
+// Send writes the packet to the underlying writer. Safe for
+// concurrent calls.
+func (s *Sender) Send(ctx context.Context, p mq.Packet) error {
+	s.Lock()
+	_, err := p.WriteTo(s.Writer)
+	s.Unlock()
+	return err
+}
