13a7514a53c87673caf31ee19049f593ac3298b5
Gregory Vincic <gregoryvse@gmail.com>
2022-10-19 22:13:35 +0200

    Remove tt/intercept

--- tt/queue_test.go
+++ tt/queue_test.go
@@ -5,13 +5,12 @@ import (
 	"testing"
 
 	"github.com/gregoryv/mq"
-	"github.com/gregoryv/mq/tt/intercept"
 )
 
 // thing is anything like an iot device that mostly sends stats to the
 // cloud
 func TestQueues(t *testing.T) {
-	recv := NewQueue([]mq.Middleware{intercept.New(0).Intercept}, mq.NoopHandler)
+	recv := NewQueue(nil, mq.NoopHandler)
 	send := NewQueue(nil, mq.NoopHandler)
 
 	ctx := context.Background()
