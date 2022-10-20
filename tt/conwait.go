5263d5a94cae77f2646f7b091209012deebb5e8d
Gregory Vincic <gregoryvse@gmail.com>
2022-10-20 20:47:23 +0200

    Add type ConnWait

---
+++ tt/conwait.go
@@ -0,0 +1,59 @@
+package tt
+
+import (
+	"context"
+	"time"
+
+	"github.com/gregoryv/mq"
+)
+
+func NewConnWait() *ConnWait {
+	return &ConnWait{}
+}
+
+type ConnWait struct {
+	connected bool
+}
+
+func (a *ConnWait) In(next mq.Handler) mq.Handler {
+	return func(ctx context.Context, p mq.Packet) error {
+		switch p.(type) {
+		case *mq.ConnAck:
+			a.connected = true
+		}
+		return next(ctx, p)
+	}
+}
+
+// Use resets on mq.Connect and counts mq.ConnAck. Must be used in both
+// in and out queues.
+func (a *ConnWait) Out(next mq.Handler) mq.Handler {
+	return func(ctx context.Context, p mq.Packet) error {
+		switch p.(type) {
+		case *mq.Connect:
+			a.connected = false
+		}
+		return next(ctx, p)
+	}
+}
+
+// AllConnscribed returns channel which blocks until expected number of
+// mq.ConnAck packets have been counted.
+func (a *ConnWait) Done(ctx context.Context) <-chan struct{} {
+	c := make(chan struct{})
+	go func() {
+		for {
+			select {
+			case <-ctx.Done():
+				close(c)
+				return
+			case <-time.After(2 * time.Millisecond):
+				if a.connected {
+					c <- struct{}{}
+					return
+				}
+			}
+		}
+	}()
+	return c
+}
