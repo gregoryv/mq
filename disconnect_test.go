aca92ccfa60d3de037220242cf5b501aa73aa5d9
Gregory Vincic <gregoryvse@gmail.com>
2022-10-23 12:58:14 +0200

    Benchmark Disconnect.UnmarshalBinary

--- disconnect_test.go
+++ disconnect_test.go
@@ -1,6 +1,9 @@
 package mq
 
-import "testing"
+import (
+	"bytes"
+	"testing"
+)
 
 func TestDisconnect(t *testing.T) {
 	p := NewDisconnect()
@@ -10,4 +13,16 @@ func TestDisconnect(t *testing.T) {
 	eq(t, p.SetReasonCode, p.ReasonCode, MalformedPacket)
 	p.AddUserProp("color", "red")
 	testControlPacket(t, &p)
+}
+
+func BenchmarkDisconnect_UnmarshalBinary(b *testing.B) {
+	p := NewDisconnect()
+	var buf bytes.Buffer
+	p.WriteTo(&buf)
+	data := buf.Bytes()[1:] // without the fixed header
+
+	var in Disconnect
+	for i := 0; i < b.N; i++ {
+		in.UnmarshalBinary(data[1:])
+	}
 }
