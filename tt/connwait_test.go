a4eec8b7a0ca212dd8907c8d409a7b9951b0c7bb
Gregory Vincic <gregoryvse@gmail.com>
2022-10-23 07:47:41 +0200

    Refactor TestConnWait

--- tt/connwait_test.go
+++ tt/connwait_test.go
@@ -10,42 +10,20 @@ import (
 
 func TestConnWait(t *testing.T) {
 	var (
-		conn, server = Dial()
-
-		sender  = NewSender(conn).Out
-		conwait = NewConnWait()
-
-		out = NewOutQueue(sender)
-		in  = NewInQueue(NoopHandler, conwait)
+		conwait     = NewConnWait()
+		in          = conwait.In(NoopHandler)
+		ctx, cancel = context.WithTimeout(
+			context.Background(), 20*time.Millisecond,
+		)
 	)
-
-	// start handling packet flow
-	ctx, cancel := context.WithTimeout(context.Background(), 20*time.Millisecond)
 	defer cancel()
-	go NewReceiver(conn, in).Run(ctx)
-
-	{ // connect
-		p := mq.NewConnect()
-		p.SetClientID("connwait-test")
-		_ = out(ctx, &p)
-		server.Ack(&p)
+	{
+		p := mq.NewConnAck()
+		go in(ctx, &p)
 	}
-
-	<-conwait.Done()
-	if err := ctx.Err(); err != nil {
-		t.Fatal(err)
-	}
-
-	// times out
-	{ // connect
-		p := mq.NewConnect()
-		p.SetClientID("connwait-timeout")
-		_ = out(ctx, &p)
-	}
-
 	select {
 	case <-conwait.Done():
-		t.Error("Done should timeout")
-	case <-time.After(time.Millisecond):
+	case <-ctx.Done():
+		t.Error(ctx.Err())
 	}
 }
