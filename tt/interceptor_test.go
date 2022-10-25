cce69da1320af5070101e4c6c0883d8d67bd6613
Gregory Vincic <gregoryvse@gmail.com>
2022-10-25 07:13:38 +0200

    Test interceptor

---
+++ tt/interceptor_test.go
@@ -0,0 +1,21 @@
+package tt
+
+import (
+	. "context"
+	"testing"
+	"time"
+
+	"github.com/gregoryv/mq"
+)
+
+func TestIntercept(t *testing.T) {
+	i := Intercept[*mq.Connect]()
+	h := i.In(NoopHandler)
+	go h(Background(), &mq.Connect{})
+
+	select {
+	case <-i.Done():
+	case <-time.After(1 * time.Millisecond):
+		t.Fail()
+	}
+}
