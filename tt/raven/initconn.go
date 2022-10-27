e577b5ab950daf891375e9a8a6bd21fabece5b62
Gregory Vincic <gregoryvse@gmail.com>
2022-10-27 03:34:50 +0200

    Refactor

---
+++ tt/raven/initconn.go
@@ -0,0 +1,48 @@
+package raven
+
+import (
+	. "context"
+	"net"
+
+	"github.com/google/uuid"
+	"github.com/gregoryv/mq"
+	"github.com/gregoryv/mq/tt"
+)
+
+// InitConn returns the client id after a successful connect and
+// ack.
+func InitConn(ctx Context, conn net.Conn) (string, error) {
+	var (
+		sender    = tt.NewSender(conn)
+		onConnect = make(chan *mq.Connect, 0)
+		connwait  = tt.Intercept(onConnect)
+		logger    = tt.NewLogger(tt.LevelInfo)
+
+		in  = tt.NewInQueue(tt.NoopHandler, connwait, logger)
+		out = tt.NewOutQueue(sender.Out, logger)
+	)
+
+	_ = out // todo register outgoing connection once connected
+	go tt.NewReceiver(conn, in).Run(ctx)
+
+	select {
+	case p := <-onConnect:
+		// connect came in...
+		a := mq.NewConnAck()
+		id := p.ClientID()
+		if id == "" {
+			id = uuid.NewString()
+		}
+		// todo make sure it's uniq
+		a.SetAssignedClientID(id)
+		if _, err := a.WriteTo(conn); err != nil {
+			return "", err
+		}
+		return id, nil
+
+	case <-ctx.Done():
+		// stopped from the outside
+		return "", ctx.Err()
+	}
+
+}
--- tt/raven/server.go
+++ tt/raven/server.go
@@ -7,10 +7,6 @@ import (
 	"net"
 	"os"
 	"time"
-
-	"github.com/google/uuid"
-	"github.com/gregoryv/mq"
-	"github.com/gregoryv/mq/tt"
 )
 
 func NewServer() *Server {
@@ -66,42 +62,4 @@ loop:
 			_ = id
 		}()
 	}
-}
-
-// InitConn returns the client id after a successful connect and
-// ack.
-func InitConn(ctx Context, conn net.Conn) (string, error) {
-	var (
-		sender    = tt.NewSender(conn)
-		onConnect = make(chan *mq.Connect, 0)
-		connwait  = tt.Intercept(onConnect)
-		logger    = tt.NewLogger(tt.LevelInfo)
-
-		in  = tt.NewInQueue(tt.NoopHandler, connwait, logger)
-		out = tt.NewOutQueue(sender.Out, logger)
-	)
-
-	_ = out // todo register outgoing connection once connected
-	go tt.NewReceiver(conn, in).Run(ctx)
-
-	select {
-	case p := <-onConnect:
-		// connect came in...
-		a := mq.NewConnAck()
-		id := p.ClientID()
-		if id == "" {
-			id = uuid.NewString()
-		}
-		// todo make sure it's uniq
-		a.SetAssignedClientID(id)
-		if _, err := a.WriteTo(conn); err != nil {
-			return "", err
-		}
-		return id, nil
-
-	case <-ctx.Done():
-		// stopped from the outside
-		return "", ctx.Err()
-	}
-
 }
