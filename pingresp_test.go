6db4b1e142f36554201978478315520f3812c81f
Gregory Vincic <gregoryvse@gmail.com>
2022-10-30 20:42:24 +0100

    Add ExamplePingResp_String

--- pingresp_test.go
+++ pingresp_test.go
@@ -1,6 +1,20 @@
 package mq
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
+
+func ExamplePingResp_String() {
+	p := NewPingResp()
+	fmt.Println(&p)
+	fmt.Print(DocumentFlags(&p))
+	// output:
+	// PINGRESP ---- 2 bytes
+	//          3210 Size
+	//
+	// 3-0 reserved
+}
 
 func TestPingResp(t *testing.T) {
 	p := NewPingResp()
