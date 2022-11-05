aa644bd773adebc55637b945c7b7f5c65a7d38f2
Gregory Vincic <gregoryvse@gmail.com>
2022-11-05 15:15:29 +0100

    Implement tt sub

--- tt/cmd/tt/sub.go
+++ tt/cmd/tt/sub.go
@@ -2,20 +2,71 @@ package main
 
 import (
 	"context"
-	"fmt"
+	"net"
 	"net/url"
 
 	"github.com/gregoryv/cmdline"
+	"github.com/gregoryv/mq"
+	"github.com/gregoryv/mq/tt"
 )
 
 type Sub struct {
-	server *url.URL
+	server      *url.URL
+	topicFilter string
 }
 
-func (s *Sub) ExtraOptions(cli *cmdline.Parser) {
-	s.server = cli.Option("-s, --server").Url("localhost:1883")
+func (c *Sub) ExtraOptions(cli *cmdline.Parser) {
+	c.server = cli.Option("-s, --server").Url("localhost:1883")
+	c.topicFilter = cli.Option("-f, --topic-filter").String("/#")
 }
 
-func (s *Sub) Run(ctx context.Context) error {
-	return fmt.Errorf("sub: todo")
+func (c *Sub) Run(ctx context.Context) error {
+	conn, err := net.Dial("tcp", c.server.String())
+	if err != nil {
+		return err
+	}
+
+	// use middlewares and build your in/out queues with desired
+	// features
+	var (
+		sender = tt.NewSender(conn)
+		pool   = tt.NewIDPool(100)
+		logger = tt.NewLogger(tt.LevelInfo)
+
+		out     = tt.NewOutQueue(sender.Out, logger, pool)
+		handler mq.Handler
+	)
+
+	// QoS dictates the logic of packet flows
+	handler = func(ctx context.Context, p mq.Packet) error {
+		switch p := p.(type) {
+		case *mq.ConnAck:
+			sub := mq.NewSubscribe()
+			sub.AddFilter(c.topicFilter, mq.OptNL)
+			return out(ctx, sub)
+
+		case *mq.Publish:
+			ack := mq.NewPubAck()
+			ack.SetPacketID(p.PacketID())
+			return out(ctx, ack)
+		}
+		return nil
+	}
+
+	var (
+		in       = tt.NewInQueue(handler, pool, logger)
+		receiver = tt.NewReceiver(in, conn)
+	)
+	// start handling packet flow
+	running := tt.Start(context.Background(), receiver)
+
+	// kick off with a connect
+	p := mq.NewConnect()
+	p.SetClientID("ttsub")
+	_ = out(ctx, p)
+
+	<-running
+
+	// todo handle ctrl+c with gracefule disconnect
+	return nil
 }
